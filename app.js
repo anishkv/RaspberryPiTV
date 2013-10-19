23dd2196c2203df43102c54db8ad8fed8e289a71
Anish <anish@zovi.com>
2013-10-19 12:31:21 +0530

    merging

--- app.js
+++ app.js
@@ -26,81 +26,101 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(omx());
+var $path;
 
-var $path = '/media/5FF5-E1EB/movies/'
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-//Routes
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/public/index.html');
-});
-
-app.get('/remote', function (req, res) {
-  res.sendfile(__dirname + '/public/remote.html');
-});
-
-app.get('/play/:video_id', function (req, res) {
-
-});
-
-app.get('/movies', function (req, res) {
-  fs.readdir($path, function(err, files){
-     return res.render('movies', {movies: files});  
-     //res.end();
-  })
-  
-});
-
-
-app.get('/poster/*', function (req, res) {
-  fs.readFile($path + req.params[0] + '/folder.jpg', function(err, data){
-    if(err) throw err;
-    res.end(data); 
+    res.sendfile(__dirname + '/public/index.html');
   });
-  
-});
 
-app.get('/fanart/*', function (req, res) {
-  
-  fs.readFile($path + req.params[0] + '/'+ req.params[0]  +'-fanart.jpg', function(err, data){
-    if(err) throw err;
-    res.end(data); 
+  app.get('/remote', function (req, res) {
+    res.sendfile(__dirname + '/public/remote.html');
   });
-  
-});
 
-app.get('/movie-details/*', function (req, res) {
+  app.get('/play/:video_id', function (req, res) {
+
+  });
+  // development only
+  if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+  }
+
+fs.readFile('config.txt', 'utf-8', function(err, data){
+  if(err) throw err;
+  $path = data.split(' ')[1]; 
   
-  fs.readFile($path + req.params[0] + '/' + req.params[0] + '.nfo', 'utf-8', function(err, data){
-    data = data.substring(1, data.length);
-    parseString(data, function (err, result) {
+
+
+  
+  //Routes
+  
+  app.get('/movies', function (req, res) {
+    fs.readdir($path, function(err, files){
+       return res.render('movies', {movies: files});  
+       //res.end();
+    })
+    
+  });
+
+  app.get('/home', function (req, res) {
+    console.log($path)
+    fs.readdir($path+ '', function(err, files){
+      console.log(err)
+       return res.render('home', {movies: files});  
+       //res.end();
+    })
+    
+  });
+
+
+  app.get('/poster/*', function (req, res) {
+    fs.readFile($path + req.params[0] + '/folder.jpg', function(err, data){
       if(err) throw err;
-      return res.render('movie-details', {title: req.params[0], rating: result.movie.rating[0], year: result.movie.year[0], plot: result.movie.plot[0]});
-      console.dir(result);
+      res.end(data); 
     });
     
+  });
+
+  app.get('/fanart/*', function (req, res) {
+    
+    fs.readFile($path + req.params[0] + '/'+ req.params[0]  +'-fanart.jpg', function(err, data){
+      if(err) throw err;
+      res.end(data); 
+    });
     
   });
-  
+
+  app.get('/movie-details/*', function (req, res) {
+    
+    fs.readFile($path + req.params[0] + '/' + req.params[0] + '.nfo', 'utf-8', function(err, data){
+      data = data.substring(1, data.length);
+      parseString(data, function (err, result) {
+        if(err) throw err;
+        return res.render('movie-details', {title: req.params[0], rating: result.movie.rating[0], year: result.movie.year[0], plot: result.movie.plot[0]});
+        console.dir(result);
+      });
+      
+      
+    });
+    
+  });
+
+
+  app.get('/play-movie/*', function (req, res) {
+    fs.readdir($path + req.params[0] , function(err, files){
+       for(i=0; i< files.length; i++) {
+        if(files[i].indexOf('.avi') > -1 || files[i].indexOf('.mp4') > -1 || files[i].indexOf('.mkv') > -1)  {
+          console.log(files[i])
+          console.log($path + req.params[0] + '/' + files[i])
+          omx.start($path + req.params[0] + '/' + files[i]);
+        }
+       }
+       //res.end();
+    })
+  });
+
 });
 
 
-app.get('/play-movie/*', function (req, res) {
-  fs.readdir($path + req.params[0] , function(err, files){
-     for(i=0; i< files.length; i++) {
-      if(files[i].indexOf('.avi') > -1 || files[i].indexOf('.mp4') > -1 || files[i].indexOf('.mkv') > -1)  {
-        console.log(files[i])
-        console.log($path + req.params[0] + '/' + files[i])
-        omx.start($path + req.params[0] + '/' + files[i]);
-      }
-     }
-     //res.end();
-  })
-});
 
 //Socket.io Config
 io.set('log level', 1);
@@ -134,7 +154,7 @@ io.sockets.on('connection', function (socket) {
  });
 
  socket.on("controll", function(data){
-	console.log(data);
+  console.log(data);
    if(socket.type === "remote"){
 
      if(data.action === "tap"){
